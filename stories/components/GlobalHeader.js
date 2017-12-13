9467eda0a6488e60307664ebd32456b850f8c446
snehanagole-osi <snagole@bitbucket.org>
2017-12-13 07:16:00 -0800

    Changes to GlobalHeader Info on storybook story

--- stories/components/GlobalHeader.js
+++ stories/components/GlobalHeader.js
@@ -12,11 +12,12 @@ const GlobalHeaderStory = withInfo(
     #### Usage
     - GlobalHeader component consists of
         * logo with optional callback
-        * search icon: visible only when search callback (*searchIconCallback* property) is provided. If *profileId* property is provided it will be passed to *searchIconCallback*.
-        * add icon: visible only when add callback (*addIconCallback* property) is provided. If *profileId* property is provided it will be passed to *addIconCallback*.
-        * notifications icon: visible only when notifications callback (*notificationIconCallback*) property is provided. If *profileId* property is provided it will be passed to *notificationIconCallback*.
+        * search
+        * add
+        * notifications
         * profileAvatar: visible when *profilId* or *logoutCallback* properties are provided. Clicking on ProfileAvatar toggles display of Logout button. Logout button is displayed only when *logoutCallback* property is provided. If *profileId* property is provided it will be passed to *logoutCallback*.
-    - GlobalHeader component depends on GlobalHeaderIcon component to render add, search, notificaitons icons.
+    - add, search, notifications (icons) are visible only when correspdoning callback properties (*addIconCallback*, *searchIconCallback*, *notificationIconCallback*). If *profileId* property is provided, it will be passed to each of these callbacks.
+    - GlobalHeader component depends on GlobalHeaderIcon component to render add, search, notificaitons functionality.
     - GlobalHeader component depends on ProfileAvatar component to render Profile Avatar.
     - Please note that GlobalHeaderIcon & ProfileAvatar components are kept as private components as they are not expected to be reused.
   `
